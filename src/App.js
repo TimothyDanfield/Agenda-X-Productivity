e0c24820290c5e457863e79cd2cdb14c78be6824
Timothy <TimothyDanfield@yahoo.com>
2023-03-01 14:19:00 -0600

    User.js, Task.js all created

--- src/App.js
+++ src/App.js
@@ -1,77 +1,10 @@
-import React, { useState } from "react";
-import { Calendar, DatePicker, Button, Input, Select } from "antd";
-import moment from "moment";
-const { Option } = Select;
+import React from 'react'
+import "./App.css"
+import Task from "./components/Task/Task"
 const App = () => {
-  const [date, setDate] = useState(moment());
-  const [taskName, setTaskName] = useState("");
-  const [category, setCategory] = useState("");
-  const [reminderTime, setReminderTime] = useState("");
-  const handleDateChange = (date) => {
-    setDate(date);
-  };
-  const handleTaskNameChange = (e) => {
-    setTaskName(e.target.value);
-  };
-  const handleCategoryChange = (value) => {
-    setCategory(value);
-  };
-  const handleReminderTimeChange = (value) => {
-    setReminderTime(value);
-  };
+  return(
+    <Task />
+  )
+}
 
-  // Create Task Functionality:
-
-  const createTask = () => {
-    if (!taskName || !category || !reminderTime) return;
-
-    let taskObj = {
-      name: taskName,
-      category: category,
-      reminderTime: reminderTime,
-      date: date.format("YYYY-MM-DD"),
-    };
-
-    console.log("Task Created", taskObj);
-
-    alert("Task Created!");
-
-    resetForm();
-  };
-
-  const resetForm = () => {
-    setDate(moment());
-
-    setTaskName("");
-
-    setCategory("");
-
-    setReminderTime("");
-  };
-  return (
-    <div>
-      <Calendar onSelect={handleDateChange} value={date} />
-      <Input placeholder="Task Name" onChange={handleTaskNameChange} />
-      <Select placeholder="Category" onChange={handleCategoryChange}>
-        <Option value="work">Work</Option>
-        <Option value="personal">Personal</Option>
-        <Option value="other">Other</Option>
-      </Select>
-      <Select placeholder="Reminder Time" onChange={handleReminderTimeChange}>
-        <Option value="1 hour before">1 hour before</Option>
-        <Option value="2 hours before">2 hours before</Option>
-        <Option value="1 day before">1 day before</Option>
-      </Select>
-      <DatePicker
-        showTime
-        format="YYYY-MM-DD HH:mm:ss"
-        placeholder="Reminder Time"
-        onChange={handleReminderTimeChange}
-      />
-      <Button type="primary" onClick={createTask}>
-        Create Task
-      </Button>
-    </div>
-  );
-};
-export default App;
+export default App
---
+++ src/components/Task/Task.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { Calendar, DatePicker, Button, Input, Select } from "antd";
+import moment from "moment";
+const { Option } = Select;
+const Task = () => {
+  const [date, setDate] = useState(moment());
+  const [taskName, setTaskName] = useState("");
+  const [category, setCategory] = useState("");
+  const [reminderTime, setReminderTime] = useState("");
+  const handleDateChange = (date) => {
+    setDate(date);
+  };
+  const handleTaskNameChange = (e) => {
+    setTaskName(e.target.value);
+  };
+  const handleCategoryChange = (value) => {
+    setCategory(value);
+  };
+  const handleReminderTimeChange = (value) => {
+    setReminderTime(value);
+  };
+
+  // Create Task Functionality:
+
+  const createTask = () => {
+    if (!taskName || !category || !reminderTime) return;
+
+    let taskObj = {
+      name: taskName,
+      category: category,
+      reminderTime: reminderTime,
+      date: date.format("YYYY-MM-DD"),
+    };
+
+    console.log("Task Created", taskObj);
+
+    alert("Task Created!");
+
+    resetForm();
+  };
+
+  const resetForm = () => {
+    setDate(moment());
+
+    setTaskName("");
+
+    setCategory("");
+
+    setReminderTime("");
+  };
+  return (
+    <div>
+      <Calendar onSelect={handleDateChange} value={date} />
+      <Input placeholder="Task Name" onChange={handleTaskNameChange} />
+      <Select placeholder="Category" onChange={handleCategoryChange}>
+        <Option value="work">Work</Option>
+        <Option value="personal">Personal</Option>
+        <Option value="other">Other</Option>
+      </Select>
+      <Select placeholder="Reminder Time" onChange={handleReminderTimeChange}>
+        <Option value="1 hour before">1 hour before</Option>
+        <Option value="2 hours before">2 hours before</Option>
+        <Option value="1 day before">1 day before</Option>
+      </Select>
+      <DatePicker
+        showTime
+        format="YYYY-MM-DD HH:mm:ss"
+        placeholder="Reminder Time"
+        onChange={handleReminderTimeChange}
+      />
+      <Button type="primary" onClick={createTask}>
+        Create Task
+      </Button>
+      
+    </div>
+  );
+};
+export default Task;
