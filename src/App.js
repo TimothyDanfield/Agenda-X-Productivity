bce7e27b10e8bd1369da38ba41918a26deb8c54b
Timothy <TimothyDanfield@yahoo.com>
2023-03-01 11:06:35 -0600

    test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Welcome <code>to the </code> LiveWires. Your website will never be better!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Nathan, Vesper, Tim
         </a>
       </header>
     </div>
