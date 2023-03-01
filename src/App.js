22dd26c0ced0c2f0b87e0ba444f5fa0ba7090542
Timothy <TimothyDanfield@yahoo.com>
2023-03-01 14:29:40 -0600

    User.js with Login, Task.js with create task all rough

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import "./App.css"
 import Task from "./components/Task/Task"
+import User from "./components/User/User"
 const App = () => {
   return(
+    <div>
     <Task />
+    <User />
+    </div>
   )
 }
 
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, Input } from "antd";
 
 const User = () => {
-  return (
-    <div>User</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-export default User
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  // Login Functionality:
+
+  const loginUser = () => {
+    if (!username || !password) return;
+
+    let userObj = { username: username, password: password };
+
+    console.log("User Logged In", userObj);
+
+    alert("User Logged In!");
+
+    resetForm();
+  };
+
+  const resetForm = () => {
+    setUsername("");
+    setPassword("");
+  };
+
+  return (
+    <div>
+      <Input placeholder="Username" onChange={handleUsernameChange} />
+      <Input placeholder="Password" onChange={handlePasswordChange} />
+      <Button type="primary" onClick={loginUser}>
+        Login
+      </Button>
+    </div>
+  );
+};
+
+export default User;
