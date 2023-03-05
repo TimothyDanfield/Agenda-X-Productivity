407f29705eb813d7af70401d69caf6c6ef965ffa
Timothy <TimothyDanfield@yahoo.com>
2023-03-05 17:12:46 -0600

    SignUp Styling

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -4,16 +4,16 @@ import './signup.css'
 const SignUp = () => {
   return (
     <div className="signup-container">
-      <h1 className="signup" >Sign Up</h1>
+      
       <form className="main">
-        <label className="label" >Name:</label>
-        <input className="input" type="text" name="name" />
+        <label className="label"  >Sign Up</label>
+        <input className="input" placeholder='Name' type="text" name="name" />
 
-        <label className="label" >Email:</label>
-        <input className="input"  type="email" name="email" />
+        <label className="label" ></label>
+        <input className="input" placeholder='Email' type="email" name="email" />
 
-        <label className="label" >Password:</label>
-        <input className="input"  type="password" name="password" />
+        <label className="label" ></label>
+        <input className="input" placeholder='Password'  type="password" name="password" />
 
         <button className="button" type="submit"  >Sign Up</button>
       </form>
