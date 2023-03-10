1ddfdd4efb0735e24d2a7a64f1f658cdc5ba253a
Timothy <TimothyDanfield@yahoo.com>
2023-03-10 01:46:55 -0600

    Aboutus Style

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -8,9 +8,9 @@ const AboutUs = () => {
     <div className='about'>
     <Card className='aboutus' >
    
-      <Title  >About Team LiveWires</Title>
-      <p >Team LiveWires is a team of three developers, Nathan Grandinette, Vesper Wetzel and Timothy Danfield. They are working together to create a productivity app that will help people stay organized and productive.</p> <br/>
-      <p >The app will provide users with a range of features such as task management, calendar integration, and reminders. It will also have an intuitive user interface that makes it easy to use. With this project, Nathan, Vesper and Timothy are aiming to make it easier for people to stay on top of their tasks and get more done.</p> 
+      <Title className='titleaboutus'  >About Team LiveWires</Title>
+      <p className='text' >Team LiveWires is a team of three developers, Nathan Grandinette, Vesper Wetzel and Timothy Danfield. They are working together to create a productivity app that will help people stay organized and productive.</p> <br/>
+      <p className='text' >The app will provide users with a range of features such as task management, calendar integration, and reminders. It will also have an intuitive user interface that makes it easy to use. With this project, Nathan, Vesper and Timothy are aiming to make it easier for people to stay on top of their tasks and get more done.</p> 
     </Card> 
 </div>
   );
