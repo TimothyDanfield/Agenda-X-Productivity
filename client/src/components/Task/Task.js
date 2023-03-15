288d388609d1cf4fa9ed32089d54f729488679da
Nathaniel Grandinette <nathaniel.grandinette@gmail.com>
2023-03-14 17:38:08 -0700

    Calendar

--- client/src/components/Task/Task.js
+++ client/src/components/Task/Task.js
@@ -1,104 +1,110 @@
 import React, { useState } from "react";
-import { Calendar, DatePicker, Button, Input, Select, Badge } from "antd";
+import { DatePicker, Button, Input, Select, Badge } from "antd";
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import format from 'date-fns/format'
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import getDay from 'date-fns/getDay'
+//import DatePicker from 'react-datepicker'
 import moment from "moment";
 import axios from '../../utils/axiosConfig'
 import './task.css'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 const { Option } = Select;
+
+const locales = {
+  "en-US": require("date-fns/locale/en-US")
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales
+})
+
+const events = [
+  {
+    title: "big Meeting",
+    allDay: true,
+    start: new Date(2023, 3, 14),
+    end: new Date(2023, 3, 16)
+  }
+]
+
 const Task = () => {
   const [date, setDate] = useState(moment());
   const [taskName, setTaskName] = useState("");
   const [category, setCategory] = useState("");
   const [reminderTime, setReminderTime] = useState("");
-  const [tasksOnCalendar, setTasksOnCalendar] = useState({});
+  const [tasksOnCalendar, setTasksOnCalendar] = useState([])
+  const [newEvent, setNewEvent] = useState({
+    title: '',
+    end: '',
+    start: '',
+    category: ''
+  })
 
-  const handleDateChange = (date) => {
-    setDate(date);
-  };
-
-  const handleTaskNameChange = (e) => {
-    setTaskName(e.target.value);
-  };
-
-  const handleCategoryChange = (value) => {
-    if (value === 'Work') { setCategory(<Badge color="green">{value}</Badge>) } else if (value === 'Personal') { setCategory(<Badge color="blue">{value}</Badge>) } else if (value === 'Completed') { setCategory(<Badge color="yellow">{value}</Badge>) } else if (value === 'Not Completed') { setCategory(<Badge color="red">{value}</Badge>) };
-  };
-
-  const handleReminderTimeChange = (value) => {
-    setReminderTime(value);
-  };
-
-  // Create Task Functionality:
-
-  const createTask = () => {
-    if (!taskName || !category || !reminderTime) return;
-
-    let taskObj = {
-      name: taskName,
-      category: category,
-      reminderTime: reminderTime,
-      date: date.format("YYYY-MM-DD"),
-    };
-    console.log("Task Created", taskObj);
-
-    alert("Task Created!");
-    let newTasksOnCalendarObj = Object.assign({}, tasksOnCalendar);
-    newTasksOnCalendarObj[date.format("YYYY-MM-DD")]
-      ? newTasksOnCalendarObj[date.format("YYYY-MM-DD")].push(taskObj)
-      : (newTasksOnCalendarObj[date.format("YYYY-MM-DD")] = [taskObj]);
-    console.log("newTasksOnCalendar", newTasksOnCalendarObj);
-
-    setTasksOnCalendar(newTasksOnCalendarObj)
-    resetForm();
-
+  const handleAddEvent = () => {
+    setTasksOnCalendar([...tasksOnCalendar, newEvent])
   }
 
-  const resetForm = () => {
-    setDate(moment());
+  const eventPropGetter = (event) => {
+    let backgroundColor
+    if (event.category === 'Personal') {
+      backgroundColor = 'green'
+    }
+    if (event.category === 'Work') {
+      backgroundColor = 'blue'
+    }
 
-    setTaskName("");
+    return { style: { backgroundColor } }
+  }
 
-    setCategory("");
+  console.log(newEvent)
 
-    setReminderTime("");
-  };
-
-
-  // add color code to different categories || add completed or not completed
   return (
-    <div>
-      <br />
+    <div className='calendar'>
+      <div>
+        <Calendar localizer={localizer} events={tasksOnCalendar}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500, margin: '50px' }}
+          eventPropGetter={eventPropGetter}
+        />
+      </div>
+      <div className='calendarForm'>
+        <Input
+          style={{ width: "200px" }}
+          onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
+          placeholder="Enter Task Name"
+        />
 
-      <br />
+        <select
+          showSearch
+          placeholder="Select a category"
+          optionFilterProp="children"
+          onChange={(value) => setNewEvent({ ...newEvent, category: value })}
+          style={{ width: "200px", borderRadius: '5px', borderColor: 'lightgray' }}
+        >
+          <option value="Work">Work</option>
+          <option value="Personal">Personal</option>
+        </select>
 
-      <Calendar onPanelChange={setDate} tasksData={tasksOnCalendar} />
-      <Input
-        value={taskName}
-        style={{ width: "200px" }}
-        onChange={handleTaskNameChange}
-        placeholder="Enter Task Name"
-      />
-      <Select
-        value={category}
-        style={{ width: "200px" }}
-        placeholder="Category"
-        onChange={handleCategoryChange}
-      >
-        <Option value="Work">Work</Option>
-        <Option value="Personal">Personal</Option>
-      </Select>
+        <DatePicker placeholder="Start Date" style={{ marginRight: '10px' }}
+          select={newEvent.start}
+          onChange={(start) => setNewEvent({ ...newEvent, start: start.toDate() })}
+        />
 
-      <Select value={reminderTime} onChange={handleReminderTimeChange}>
-        <Option style={{ width: "200px" }} value="1 hour before">
-          1 hour before
-        </Option>
-        <Option style={{ width: "200px" }} value="2 hours before">
-          2 hours before
-        </Option>
-      </Select>
-      <DatePicker defaultValue={moment()} onChange={handleDateChange} />
-      <Button type="primary" onClick={createTask}>
-        Create Task
-      </Button>
+        <DatePicker placeholder="End Date"
+          select={newEvent.end} onChange={(end) => setNewEvent({ ...newEvent, end: end.toDate() })} />
+
+        <Button type="primary" onClick={handleAddEvent} style={{ marginLeft: '10px' }}>
+          Create Task
+
+        </Button>
+      </div>
     </div>
 
   );
