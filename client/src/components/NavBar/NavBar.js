cc84d5113d286784c1bea6c5ed8a5d3725875152
Nathaniel Grandinette <nathaniel.grandinette@gmail.com>
2023-04-10 12:01:25 -0700

    Fixed navbar issue on mobile

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, } from "react-router-dom";
 import { useNavigate } from 'react-router-dom'
 import { googleLogout } from "@react-oauth/google";
 import MusicPlayer from '../MusicPlayer/MusicPlayer'
+import { Spin as Hamburger } from 'hamburger-react'
 import './navbar.css'
 
 
 
 const NavBar = () => {
+
+  const [toggle, setToggle] = useState(false)
 
 
   const navigate = useNavigate()
@@ -21,17 +24,43 @@ const NavBar = () => {
   return (
     <div className="navbar1">
       {user ?
-        <div className="navbar2">
-          <span style={{ color: 'white' }}>{`Welcome, ${user.name}`}
-          <MusicPlayer />
-          </span>
-          
+        <div>
+          <div className="navbar2">
+            <span style={{ color: 'white' }}>{`Welcome, ${user.name}`}</span>
+            <MusicPlayer />
+            <div>
+              <Hamburger color='white' onToggle={setToggle} />
+            </div>
+          </div>
+          <div>
+
+            {toggle ?
+              <div className='links'>
+                <Link to="/task" className='link'>
+                  Work
+                </Link>
+                <Link to="/notes" className='link'>
+                  Notes
+                </Link>
+                <Link to="/aboutus" className='link'>
+                  About Us
+                </Link>
+                <Link to="/contact" className='link'>
+                  Contact Us
+                </Link>
+                <Link to="/profile" className='link'>
+                  Profile
+                </Link>
+
+                <button onClick={handleLogout} className='logout-button'>Logout</button>
+              </div> : ''}
+          </div>
+        </div> :
+        <div className="navbar3">
+          <span style={{ color: 'white' }}>{`Welcome`}</span>
           <div className='links'>
-            <Link to="/task" className='link'>
-              Work
-            </Link>
-            <Link to="/notes" className='link'>
-              Notes
+            <Link to="/home" className='link'>
+              Home
             </Link>
             <Link to="/aboutus" className='link'>
               About Us
@@ -39,29 +68,10 @@ const NavBar = () => {
             <Link to="/contact" className='link'>
               Contact Us
             </Link>
-            <Link to="/profile" className='link'>
-              Profile
-            </Link>
-          </div>
-          <div style={{maxHeight: '5px'}}>
-            <button onClick={handleLogout} className='logout-button'>Logout</button>
-          </div>
-        </div> :
-        <div className="navbar3">
-          <span style={{ color: 'white' }}>{`Welcome`}</span>
-          <div className='links'>
-          <Link to="/home" className='link'>
-            Home
-          </Link>
-          <Link to="/aboutus" className='link'>
-            About Us
-          </Link>
-          <Link to="/contact" className='link'>
-            Contact Us
-          </Link>
           </div>
           <div></div>
-        </div>}
+        </div>
+      }
     </div>
   );
 };
